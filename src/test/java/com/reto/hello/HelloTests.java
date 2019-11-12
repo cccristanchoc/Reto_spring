a32cc266f2393f965ec8f863ffd94205fae92f08
cccristanchoc <cccristanchoc@unal.edu.co>
2019-11-11 20:26:10 -0500

    Added project Reto_Spring build in Spring Boot

---
+++ src/test/java/com/reto/hello/HelloTests.java
@@ -0,0 +1,13 @@
+package com.reto.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
